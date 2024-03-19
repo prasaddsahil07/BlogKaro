73f3a0013113f5af5492e3d1c0b9faf9e02e64b2
Sahil Prasad <sahilprasad41969@gmailcom>
2024-03-19 12:24:25 +0530

    signup and login form done

---
+++ src/components/Select.jsx
@@ -0,0 +1,29 @@
+import React, {useId} from 'react'
+
+function Select({
+    options,            // options array
+    label,
+    className="",
+    ...props
+}, ref) {
+    const id = useId()
+  return (
+    <div className='w-full'>
+        {label && <label htmlFor={id} className=''></label>}
+        <select
+        {...props}
+        id={id}
+        ref={ref}
+        className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
+        >
+            {options?.map((option) => (          // if "options" is null then map will crash so use ternary operator
+                <option key={option} value={option}>
+                    {option}
+                </option>
+            ))}
+        </select>
+    </div>
+  )
+}
+
+export default React.forwardRef(Select)         // this is also an syntax 

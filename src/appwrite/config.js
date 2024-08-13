80bd7b5d33bdaa08c0bd1005ad945abc2dd12ccc
prasaddsahil07 <122683620+prasaddsahil07@users.noreply.github.com>
2024-08-13 20:58:44 +0530

    Update config.js

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -69,7 +69,7 @@ export class Service {
         }
       );
     } catch (error) {
-      console.log("Appwrite serive :: updatePost :: error", error);
+      console.log("Appwrite service :: updatePost :: error", error);
     }
   }
 
@@ -82,7 +82,7 @@ export class Service {
       );
       return true;
     } catch (error) {
-      console.log("Appwrite serive :: deletePost :: error", error);
+      console.log("Appwrite service :: deletePost :: error", error);
       return false;
     }
   }
@@ -95,7 +95,7 @@ export class Service {
         slug
       );
     } catch (error) {
-      console.log("Appwrite serive :: getPost :: error", error);
+      console.log("Appwrite service :: getPost :: error", error);
       return false;
     }
   }
@@ -108,7 +108,7 @@ export class Service {
         queries
       );
     } catch (error) {
-      console.log("Appwrite serive :: getPosts :: error", error);
+      console.log("Appwrite service :: getPosts :: error", error);
       return false;
     }
   }
@@ -140,7 +140,7 @@ export class Service {
         file
       );
     } catch (error) {
-      console.log("Appwrite serive :: uploadFile :: error", error);
+      console.log("Appwrite service :: uploadFile :: error", error);
       return false;
     }
   }
@@ -150,7 +150,7 @@ export class Service {
       await this.bucket.deleteFile(conf.appwriteBucketId, fileId);
       return true;
     } catch (error) {
-      console.log("Appwrite serive :: deleteFile :: error", error);
+      console.log("Appwrite service :: deleteFile :: error", error);
       return false;
     }
   }

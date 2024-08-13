645cb1237b8783861a4f822ce84e705c37ed98a7
prasaddsahil07 <122683620+prasaddsahil07@users.noreply.github.com>
2024-08-13 19:20:13 +0530

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -41,6 +41,11 @@ function Header() {
       active: authStatus,
     },
     {
+      name: "My Posts",
+      slug: "/my-posts",
+      active: authStatus,
+    },
+    {
       name: "Add Post",
       slug: "/add-post",
       active: authStatus,

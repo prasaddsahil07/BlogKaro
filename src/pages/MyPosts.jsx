c7b0119d24848372c34b63be24c969a686b69201
prasaddsahil07 <122683620+prasaddsahil07@users.noreply.github.com>
2024-08-14 19:23:51 +0530

    Update MyPosts.jsx

--- src/pages/MyPosts.jsx
+++ src/pages/MyPosts.jsx
@@ -7,7 +7,7 @@ import '../index.css'; // Ensure to import the CSS file where spinner styles are
 function MyPosts() {
     const [posts, setMyPosts] = useState([]);
     const [loading, setLoading] = useState(true);
-    const userId = "6659f09b187b2cc98542";
+    const userId = "66bb7cf51522713c699e";
 
     useEffect(() => {
         appwriteService.getMyPosts(userId).then((posts) => {
